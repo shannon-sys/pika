2a9e33f45f37fe99e1e2ca448902d758a9b10572
Jinghui Fu <jinghui.fu@shannon-sys.com>
2019-10-28 17:16:18 +0800

    update pikatests.sh by wenliang
    Signed-off-by: Jinghui Fu <jinghui.fu@shannon-sys.com>

--- pikatests.sh
+++ pikatests.sh
@@ -5,6 +5,8 @@ cp output/bin/pika src/redis-server
 cp output/conf/pika.conf tests/assets/default.conf
 
 tclsh tests/test_helper.tcl --clients 1 --single unit/$1
+tclreturn=$?
 rm src/redis-server
 rm -rf ./log
 rm -rf ./db
+[[ ${tclreturn} -eq "0" ]] || exit 1
